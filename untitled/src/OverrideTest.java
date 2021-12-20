67bbb43c14f7d37c11ebe01fe676a9501e3bb014
sdj3261 <63071967+sdj3261@users.noreply.github.com>
2021-12-20 23:33:58 +0900

    2021/12/18 Java Algorithm

---
+++ untitled/src/OverrideTest.java
@@ -0,0 +1,34 @@
+public class OverrideTest {
+
+    public static void main() {
+
+        new OverrideTest().test();
+    }
+
+    private void test() {
+        SuperClass a = new SubClass();
+        a.doh(1);
+    }
+
+    class SuperClass {
+
+        public char doh(char c) {
+            System.out.println("doh(char)");
+            return 'c';
+        }
+
+        public float doh(float f) {
+            System.out.println("doh(float)");
+            return 1.0f;
+        }
+    }
+
+    class OtherClass { }
+
+    class SubClass extends SuperClass {
+
+        public void doh(OtherClass o) {
+            System.out.println("doh(OtherClass)");
+        }
+    }
+}
