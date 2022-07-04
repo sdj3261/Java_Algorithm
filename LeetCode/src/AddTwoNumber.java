cda0afcc96674593e8b022eaa9b84d7ed70dc357
sdj3261 <63071967+sdj3261@users.noreply.github.com>
2022-07-04 23:17:15 +0900

    2022/07/04 LeetCode Java Algorithm - AddTwoNumber

---
+++ LeetCode/src/AddTwoNumber.java
@@ -0,0 +1,52 @@
+public class AddTwoNumber {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    static class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode node = new ListNode(0);
+            ListNode result = node;
+            int sum = 0;
+
+            while (l1 != null || l2 != null || sum > 0) {
+                if (l1 != null) {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+
+                if (l2 != null) {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                node.next = new ListNode(sum % 10);
+                sum /= 10;
+
+                node = node.next;
+            }
+
+            return result.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution a = new Solution();
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(9)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4, new ListNode(9))));
+        System.out.println(a.addTwoNumbers(l1, l2));
+    }
+}
